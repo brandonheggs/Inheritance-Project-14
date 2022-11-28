d1db8a5bf8c614b784351659fa5f13ffca5d21b7
Kris Crona <brandon.heggs@yahoo.com>
2022-11-28 15:47:06 -0500

    Made prototypes and defined member variables in Car.h while also including Vehicle.h for the Vehicle class.

--- Project12/Car.h
+++ Project12/Car.h
@@ -4,12 +4,23 @@
 #ifndef CAR_H
 #define CAR_H
 #include <iostream>
+#include "Vehicle.h"
 #pragma once
 
 using namespace std;
 
 class Car : public Vehicle
-{ 
+{  
+private: 
+	int numDoors;
+	
+public:
+	Car(string manufacturer, int yearBuilt, int numDoors);
+
+	int getNumDoors();
+	void setNumDoors(int numDoors);
+	
+	void displayInfo();
 
 };
 
