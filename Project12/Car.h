0b896b304d6ec6f1e829bea347d8d56c887171c7
Kris Crona <brandon.heggs@yahoo.com>
2022-11-28 15:41:05 -0500

    Making the Car class in Car.H as well as setting up the Header file.

--- Project12/Car.h
+++ Project12/Car.h
@@ -1,5 +1,20 @@
 // Brandon Heggs
 //CIS 1202 501
 //November 26, 2022
+#ifndef CAR_H
+#define CAR_H
+#include <iostream>
 #pragma once
+
+using namespace std;
+
+class Car : public Vehicle
+{ 
+
+};
+
+
+
+
+#endif
 
