3710b22ce36e3ac1cb25207d2a915c248b9519e7
Kris Crona <brandon.heggs@yahoo.com>
2022-11-28 02:40:05 -0500

    Making methods for Vehicle.cpp

--- Project12/Vehicle.cpp
+++ Project12/Vehicle.cpp
@@ -1,3 +1,36 @@
 // Brandon Heggs
 //CIS 1202 501
 //November 26, 2022
+
+#include "Vehicle.h"
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include<fstream>
+#include <cstdlib>
+
+using namespace std;
+
+Vehicle::Vehicle(string manufacturer, int yearBuilt)
+{ 
+
+}
+
+string Vehicle::getManufacturer()
+{
+
+}
+
+void Vehicle::setManufacturer(string manufacturer)
+{
+
+}
+int Vehicle::getYearBuilt()
+{
+
+}
+
+void Vehicle::displayInfo()
+{
+
+}
