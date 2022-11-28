e3940d9022ae3981b1e0a831f259e2dcb3ab2370
Kris Crona <brandon.heggs@yahoo.com>
2022-11-28 16:03:00 -0500

    Making the methods for Car.cpp to use the header file to call the getters and setters and class while including Vehicle class.

---
+++ Project12/Car.cpp
@@ -0,0 +1,28 @@
+// Brandon Heggs
+//CIS 1202 501
+//November 26, 2022
+#include <iostream>
+#include "Car.h"
+#include "Vehicle.h"
+#include "Vehicle.cpp"
+
+using namespace std;
+
+Car::Car(string manufacturer, int yearBuilt, int num) : numDoors(num), Vehicle(manufacturer, yearBuilt)
+{
+
+}
+int Car::getNumDoors()
+{
+
+}
+
+void Car::setNumDoors(int numDoors)
+{ 
+
+}
+
+void Car::displayInfo()
+{
+
+}
