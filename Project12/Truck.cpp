0988b776191ae25a2e43a3591bdb4a6abb4cae24
Kris Crona <brandon.heggs@yahoo.com>
2022-11-28 19:43:49 -0500

    Making class  methods for the getter and setters  as well as displayInfo

---
+++ Project12/Truck.cpp
@@ -0,0 +1,30 @@
+// Brandon Heggs
+//CIS 1202 501
+//November 26, 2022
+#include <iostream>
+#include "Truck.h"
+#include "Car.h"
+#include "Vehicle.h"
+#include "Vehicle.cpp"
+
+using namespace std;
+
+Truck::Truck(string manufactor, int year, int towing) : towingCapacity(towing), Vehicle(manufactor, year)
+{
+
+}
+
+int Truck::getTowingCapacity()
+{
+
+}
+
+void Truck::setTowingCapacity(int towing)
+{
+
+}
+
+void Truck::displayInfo()
+{
+
+}
